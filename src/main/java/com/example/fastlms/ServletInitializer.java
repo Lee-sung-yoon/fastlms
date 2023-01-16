597e453ded41d4e00c4de7f65ef753c712c9defb
LEE <tjddbs1412@naver.com>
2023-01-16 09:36:03 +0900

    fastlms 초기화 설정 - 2

--- src/main/java/com/example/fastlms/ServletInitializer.java
+++ src/main/java/com/example/fastlms/ServletInitializer.java
@@ -3,6 +3,8 @@ package com.example.fastlms;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+//2021.08.23
+
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
